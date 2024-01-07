b100c30e3cd1da193e76bc8700c0c501e1c15525
sahilxvaish <130292899+sahilxvaish@users.noreply.github.com>
2024-01-07 02:24:40 +0000

    variables and datatypes

---
+++ .devcontainer/basics_01/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "sahil@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+ accountEmail = "sv@sv.com"
+ accountPassword = "21212121"
+ accountCity = "Bengaluru"
+
+ console.log(accountId);
+
+
+ console.table([accountId, accountEmail, accountPassword, accountCity])
---
+++ .devcontainer/basics_01/02_datatypes.js
@@ -0,0 +1,35 @@
+"use strict" // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3
+    +3) //code readability should be high 
+
+console.log("Sahil")
+
+//ECMAscript
+
+let name = "Sahil" //string datatype
+let age = 21 // number datatype
+let isLoggedIn = false //boolean datatype 
+
+
+//DATATYPES
+
+// PRIMITIVE DATATYPES
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true or false 
+// null => standalone value 
+// undefined => variable declared but not given the value. 
+// symbol => unique 
+
+//NON PRIMITIVE DATATYPES 
+
+//object 
+
+console.log(typeof "SAHIL")
+console.lof(typeof undefined); // undefined 
+console.log(typeof  null); //object
